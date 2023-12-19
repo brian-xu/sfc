3c29fc82a23490554c4ebcbac9e82f3cddc0e8f6
Brian Xu <xu.brian.s@gmail.com>
2023-12-18 17:53:18 -0800

    update curve generation

--- gen_curve.py
+++ gen_curve.py
@@ -378,72 +378,132 @@ def grid(columns, rows):
     return dims
 
 
-def gen_curve(w, h, d=None):
-    if w == 1:
-        if h == 1:
-            return True
-        if d[2] in ["0", "2"]:
-            return False
-        else:
-            return True
-    if h == 1:
-        if d[2] in ["1", "3"]:
-            return False
-        else:
-            return True
-    if w == 2:
-        if h % 2 == 0:
-            return d[0] == "s"
-        if h % 2 == 1:
-            return d[2] not in ["1", "3"]
-    if h == 2:
-        if w % 2 == 0:
-            return d[0] == "s"
-        if w % 2 == 1:
-            return d[2] not in ["0", "2"]
-    # w_parity = number of even-width columns
-    w_div = w // 3
-    w_mod = w % 3
-    if w_div % 2 == 0:
-        w_parity = 3 - w_mod
+def gen_curve(width, height, d):
+    if width == 1:
+        if height == 1:
+            return [np.array([[0]]), False]
+        if d in ["s_0", "s_0_r", "s_2", "s_2_r"]:
+            return [np.array([]), False]
+        if d in ["s_1", "s_3_r", "d_neg", "d_pos_r"]:
+            return [np.array([[i] for i in range(height)]), False]
+        if d in ["s_3", "s_1_r", "d_pos", "d_neg_r"]:
+            return [np.array([[height-i-1] for i in range(height)]), False]
+    if height == 1:
+        if d in ["s_1", "s_1_r", "s_3", "s_3_r"]:
+            return [np.array([]), False]
+        if d in ["s_0", "s_2_r", "d_neg", "d_pos"]:
+            return [np.array([[i for i in range(width)]]), False]
+        if d in ["s_2", "s_0_r", "d_neg_r", "d_pos_r"]:
+            return [np.array([[width-i-1 for i in range(width)]]), False]
+    if width == 2:
+        if height % 2 == 0:
+            if d in ["d_neg", "d_neg_r", "d_pos", "d_pos_r"]:
+                return [np.array([]), False]
+            else:
+                return [np.array([[i, 2*height-i-1] for i in range(height)]), True]
+        if height % 2 == 1:
+            if d in ["s_1", "s_1_r", "s_3", "s_3_r"]:
+                return [np.array([]), False]
+            if d in ["s_0", "s_0_r", "s_2", "s_2_r"]:
+                return [np.array([[i, 2*height-i-1] for i in range(height)]), True]
+            if d in ["d_neg", "d_neg_r"]:
+                return [np.array([[2*i+1*(i%2), 2*i+1*(1-i%2)] for i in range(height)]), True]
+            if d in ["d_pos", "d_pos_r"]:
+                return [np.array([[2*i+1*(1-i%2), 2*i+1*(i%2)] for i in range(height)]), True]
+    if height == 2:
+        if width % 2 == 0:
+            if d in ["d_neg", "d_neg_r", "d_pos", "d_pos_r"]:
+                return [np.array([]), False]
+            else:
+                return [np.array([[i for i in range(width)], [2*width-i-1 for i in range(width)]]), True]
+        if width % 2 == 1:
+            if d in ["s_0", "s_0_r", "s_2", "s_2_r"]:
+                return [np.array([]), False]
+            if d in ["s_1", "s_1_r", "s_3", "s_3_r"]:
+                return [np.array([[i, 2*height-i-1] for i in range(height)]), True]
+            if d in ["d_neg", "d_neg_r"]:
+                return [np.array([[i*2 + 1*(i%2) for i in range(width)], [i*2 + 1*(1-i%2) for i in range(width)]]), True]
+            if d in ["d_pos", "d_pos_r"]:
+                return [np.array([[i*2 + 1*(1-i%2) for i in range(width)], [i*2 + 1*(i%2) for i in range(width)]]), True]
+    # width_parity = number of even-width columns
+    width_div = width // 3
+    width_mod = width % 3
+    if width_div % 2 == 0:
+        width_parity = 3 - width_mod
     else:
-        w_parity = w_mod
-    h_div = h // 3
-    h_mod = h % 3
-    if h_div % 2 == 0:
-        h_parity = 3 - h_mod
+        width_parity = width_mod
+    height_div = height // 3
+    height_mod = height % 3
+    if height_div % 2 == 0:
+        height_parity = 3 - height_mod
     else:
-        h_parity = h_mod
-    offset, sol = curve_map[w_parity][h_parity]
-    if d:
-        if d[0] == "d" and "Z" not in sol:
-            return False
-        if d[0] == "s" and "G" not in sol:
-            return False
-    w_offset, h_offset = offset
-    if w_div % 2 == 1:
-        if w_mod == 0:
-            w_dim = w_offset + w_div - 1
+        height_parity = height_mod
+    offset, solution = curve_map[width_parity][height_parity]
+    if d[0] == "d":
+        if "Z" not in solution:
+            return [np.array([]), False]
         else:
-            w_dim = (w_div + 1) - w_offset
-    else:
-        w_dim = w_offset + w_div
-    if h_div % 2 == 1:
-        if h_mod == 0:
-            h_dim = h_offset + h_div - 1
+            order = ordered_z
+            solution = solution["Z"]
+    if d[0] == "s":
+        if "G" not in solution:
+            return [np.array([]), False]
         else:
-            h_dim = (h_div + 1) - h_offset
+            order = ordered_g
+            solution = solution["G"]
+    width_offset, height_offset = offset
+    if width_div % 2 == 1:
+        if width_mod == 0:
+            width_dim = width_offset + width_div - 1
+        else:
+            width_dim = (width_div + 1) - width_offset
     else:
-        h_dim = h_offset + h_div
-    subsections = grid(w_dim, h_dim)
-    if not d or d[0] == "d":
-        for i in range(9):
-            if not gen_curve(*subsections[ordered_z[i][0]][ordered_z[i][1]], sol["Z"][i]):
-                print(subsections[ordered_z[i][0]][ordered_z[i][1]], sol["Z"][i])
-                return False
-    if not d or d[0] == "s":
-        for i in range(9):
-            if not gen_curve(*subsections[ordered_g[i][0]][ordered_g[i][1]], sol["G"][i]):
-                print(subsections[ordered_g[i][0]][ordered_g[i][1]], sol["G"][i])
-                return False
-    return True
+        width_dim = width_offset + width_div
+    if height_div % 2 == 1:
+        if height_mod == 0:
+            height_dim = height_offset + height_div - 1
+        else:
+            height_dim = (height_div + 1) - height_offset
+    else:
+        height_dim = height_offset + height_div
+    subsections = grid(width_dim, height_dim)
+    res = [[0 for _ in range(3)] for __ in range(3)]
+    for n in range(9):
+        i, j = order[n]
+        subsection_direction = solution[n]
+        curve_direction = subsection_direction
+        subsection_width, subsection_height = subsections[i][j]
+        if subsection_direction[2] in ["1", "3"]:
+            if subsection_width > 2 and subsection_height > 2:
+                subsection_width, subsection_height = subsection_height, subsection_width
+                curve_direction = "s_0"
+                if subsection_direction[-1] == "r":
+                    curve_direction += "_r"
+        if subsection_direction[2] == "p":
+            if subsection_width > 2 and subsection_height > 2:
+                subsection_width, subsection_height = subsection_height, subsection_width
+                curve_direction = "d_neg"
+                if subsection_direction[-1] == "r":
+                    curve_direction += "_r"
+        subsection, needs_rotation = gen_curve(subsection_width, subsection_height, curve_direction)
+        if len(subsection) == 0:
+            return [np.array([]), False]
+        if needs_rotation:
+            rotations = subsection_direction[2]
+            if rotations == "p":
+                subsection = np.rot90(subsection, k=1)
+            if rotations.isnumeric():
+                subsection = np.rot90(subsection, k=-int(rotations))
+            if subsection_direction[-1] == "r":
+                subsection = subsection.size-1-subsection
+        res[i][j] = subsection
+    prev = 0
+    for n in range(9):
+        i, j = order[n]
+        res[i][j] += prev
+        prev += res[i][j].size
+    res = np.vstack([np.hstack(res[0]),np.hstack(res[1]),np.hstack(res[2])])
+    return [res, True]
+
+x, _ = gen_curve(9, 9,"s_0")
+print(x)
